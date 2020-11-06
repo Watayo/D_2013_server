2fce2a1d5a233cf9dfee7a9351830801ea00366a
Watayo <ryo0616mani@gmail.com>
2020-11-06 11:37:38 +0900

    testcode

--- server/routes/index.js
+++ server/routes/index.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/setting_game', (req, res) => {
+router.get('/setting_game/:id', (req, res) => {
   // 1. expo clientから受け取る.
   // firestoreでゲーム振り分け
   const time_manegement = (time) => {
@@ -69,6 +69,8 @@ router.get('/setting_game', (req, res) => {
   });
 });
 
+
+
 router.post('/unity_score', (req, res) => {
   // unityHTTPRequestでうけとる
   // req.body.user_score = 1(clear) || 0(failure) 的な感じ
@@ -76,9 +78,64 @@ router.post('/unity_score', (req, res) => {
   // reactへ
 });
 
+
+
 // test Setting_game
 router.get('/test_setting_game', (req, res) => {
+  console.log(req.query.uid);
+  // 1. expo clientから受け取る.
+  // firestoreでゲーム振り分け
+  const time_manegement = (time) => {
+    let t = {
+      hour: 0,
+      minute: 0,
+      second: 0,
+    };
+    t.hour = time.getHours();
+    t.minute = time.getMinutes();
+    t.second = time.getSeconds();
+    return t;
+  };
 
+  const compare_time = (t, t2) => {
+    let T = t.minute * 60 + t.second;
+    let T2 = t2.minute * 60 + t2.second;
+    let compare = Math.abs(T - T2);
+    return compare;
+  }
+
+  let eventsRef = db.collection('events');
+
+  eventsRef.where('uid', '==', req.query.uid).limit(1).get().then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return
+    }
+    snapshot.forEach((doc) => {
+      // console.log(doc.data());
+      let t = time_manegement(new Date(doc.data().get_up_time));
+
+      let t_hope = time_manegement(new Date(doc.data().getup_hope_time));
+
+      let diff = compare_time(t, t_hope) / 60.0;
+
+      if (diff <= 10.0) {
+        // unityRendering
+        // 3. unityWebGLページを返す
+        console.log("sucess!" + diff);
+      } else {
+        // form
+        console.log("failure" + diff);
+      }
+    })
+  }).catch((err) => {
+    console.log('Error getting documents', err);
+  });
+});
+
+// test unity build
+router.get('/test_unity_build', (req, res) => {
+  res.render('unity_build');
 });
 
 // test Unity_score
